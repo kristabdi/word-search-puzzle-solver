60b314e8f96f3750148910b482d33385a1dfb32d
Zeus-s <kristabdixx@gmail.com>
2022-01-21 23:51:07 +0700

    init

---
+++ src/main.cpp
@@ -0,0 +1,127 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <chrono>
+using namespace std;
+using namespace std::chrono;
+
+typedef vector<vector<char>> matrix;
+
+matrix loadMatrix(ifstream& file);
+vector<string> loadAnswers(ifstream& file);
+void printMatrix(matrix mat);
+void check(matrix mat, string answer, int r, int c, int dr, int dc);
+string clean(string s);
+
+int counter;
+
+int main() {
+    string filename;
+    cout << "Masukkan nama file : ";
+    cin >> filename;
+    ifstream file(filename.c_str());
+    matrix board;
+    board = loadMatrix(file);
+    printMatrix(board);
+    vector<string> answers = loadAnswers(file);
+    auto start = high_resolution_clock::now();
+    for (const string& answer : answers) {
+        for (int i = 0; i < board.size(); i++) {
+            for (int j = 0; j < board[i].size(); j++) {
+                for (int dr = -1; dr <= 1; dr++) {
+                    for (int dc = -1; dc <= 1; dc++) {
+                        if (dr == 0 && dc == 0) continue;
+                        check(board, answer, i, j, dr, dc);
+                    }
+                }
+            }
+        }
+    }
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<microseconds>(stop - start);
+    cout << "Time taken by program: " << duration.count() << " microseconds" << endl;
+    cout << "Jumlah total perbandingan huruf yang dilakukan untuk menemukan kata di dalam puzzle: " << counter << " kali" << endl;
+    file.close();
+}
+
+matrix loadMatrix(ifstream& file) {
+    string line;
+    matrix mat;
+    while (getline(file, line)) {
+        line = clean(line);
+        if (line.length() == 0) break; //EOF
+
+        vector<char> row;
+        for (const char& cc : line) {
+            if (cc >= 'A' && cc <= 'Z') {
+                row.push_back(cc);
+            }
+        }
+        mat.push_back(row);
+    }
+    
+    return mat;
+}
+
+vector<string> loadAnswers(ifstream& file) {
+    vector<string> answers;
+    string line;
+    while (getline(file, line)) {
+        if (line.length() == 0) break;
+        answers.push_back(clean(line));
+    }
+    return answers;
+};
+
+void printMatrix(matrix mat) {
+    for (int i = 0; i < mat.size(); i++) {
+        for (int j = 0; j < mat[i].size(); j++) {
+            cout << mat[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+void check(matrix mat, string answer, int r, int c, int dr, int dc) {
+    bool correct = true;
+    matrix res = mat;
+    matrix temp = mat;
+    for (int i = 0; i < answer.length(); i++) {
+        int ri = r + dr * i;
+        int ci = c + dc * i;
+        if (
+            ri < 0 || ri >= mat.size() || // overflow vertically
+            ci < 0 || ci >= mat[ri].size() || // overflow horizontally
+            mat[ri][ci] != answer[i]
+        ) {
+            correct = false;
+            break;
+        } else {
+            temp[ri][ci] = '1';
+        }
+        counter++;
+    }
+
+    if (!correct) {
+        return;
+    }
+
+    for (int i = 0; i < mat.size(); i++) {
+        for (int j = 0; j < mat[i].size(); j++) {
+            if (temp[i][j] != '1') {
+                res[i][j] = '-';
+            }
+        }
+    }
+    printMatrix(res);
+    cout << endl;
+}
+
+string clean(string s) {
+    // sometimes the file can have CRLF ending, so we need to clean the string first before using it.
+    if (s.length() == 0) return s;
+    if (s[s.length()-1] != '\x0D') return s;
+    return s.substr(0, s.length()-1);
+}
